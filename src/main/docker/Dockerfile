655c625e297288550114c0d76593be5d91dc41af
Sanjay Rawat <sanjayprawat31@gmail.com>
2017-05-06 19:12:05 +0530

    Add Error Pages and Dockerfile

---
+++ src/main/docker/Dockerfile
@@ -0,0 +1,6 @@
+FROM java:8
+VOLUME /tmp
+ADD springboot-adminlte-security-0.1.1.jar app.jar
+RUN sh -c 'touch /app.jar'
+ENV JAVA_OPTS=""
+ENTRYPOINT [ "sh", "-c", "java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -jar /app.jar" ]
--- src/main/java/com/rawsanj/adminlte/web/controller/DashboardController.java
+++ src/main/java/com/rawsanj/adminlte/web/controller/DashboardController.java
@@ -1,7 +1,9 @@
 package com.rawsanj.adminlte.web.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Sanjay on 8/15/2016.
@@ -13,6 +15,12 @@ public class DashboardController {
     @RequestMapping("/")
     public String index() {
         return "dashboard/index";
+    }
+
+    // Added to test 500 page
+    @RequestMapping(path = "/tigger-error", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void error500() throws Exception {
+        throw new Exception();
     }
 
 }
